8ba78f3b653c51fa207298e8b02f94bbe57b65a5
Mrymsarabi <mrymsarabi@gmail.com>
2025-10-18 21:05:09 +0330

    adding button and input to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="p-10 space-y-6 bg-[var(--color-background)] text-[var(--color-foreground)]">
+      <div className="space-x-3">
+        <Button>Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="ghost">Ghost</Button>
+      </div>
+
+      <Input placeholder="Type something..." />
+    </main>
   );
 }
