b4074b24c9ea704656bae9043e7af2138bb0b2b5
Mrymsarabi <mrymsarabi@gmail.com>
2025-10-18 21:03:57 +0330

    components - input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-const Input = () => {
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type = "text", ...props }, ref) => {
     return (
-        <div>
-
-        </div>
+      <input
+        type={type}
+        ref={ref}
+        className={cn(
+          "flex h-10 w-full rounded-[var(--radius-card)] border border-[var(--color-foreground)]/20 bg-[var(--color-background)] px-3 py-2 text-sm text-[var(--color-foreground)] shadow-[var(--shadow)] placeholder:text-[var(--color-foreground)]/50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[var(--color-primary)] focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-60",
+          className
+        )}
+        {...props}
+      />
     );
-};
-
-export default Input;
+  }
+);
+Input.displayName = "Input";
